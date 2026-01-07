a357f6e73c7f311fab448e0d905cf47c159eae8b
Vector-mayu <mayureshdandekar143@gmail.com>
2026-01-07 10:20:41 +0530

    Added CuisineCard and MenuCard Swiggy Project

--- DAY17/src/Components/MenuCard.jsx
+++ DAY17/src/Components/MenuCard.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import CuisineCard from "./CuisineCard"
 
 export default function MenuCard({MenuItems}){
+
+	const [IsOpen, setIsOpen] = useState(true);
 	
 	if(MenuItems?.categories?.length in MenuItems)
 	{
@@ -17,14 +20,35 @@ export default function MenuCard({MenuItems}){
 		)
 	}
 	
+	if(!IsOpen)
+    {
+        return(
+            <div className="w-full mt-14">
+                <div className="flex justify-between w-full">
+                    <p className="text-2xl font-bold ">{MenuItems.title}</p>
+                    <button className="text-5xl mr-10" onClick={()=> setIsOpen(!IsOpen)}>{IsOpen? `▾`:`▴`}</button>
+                </div>
+                <div className="h-4 bg-gray-200 w-full"></div>
+            </div>
+        )
+    }
+
 	return(
-		<div className="w-full mt-14">
-			<p className="text-2xl font-bold ">{MenuItems.title}</p>
+		<div className="w-full mt-15 mb-15">
+
+			<div className="flex justify-between w-full">
+			    <p className="text-2xl font-bold ">{MenuItems.title}</p>
+                <button className="text-3xl mr-10" onClick={()=> setIsOpen(!IsOpen)}>{IsOpen? `▾`:`▴`}</button>
+            </div>
+
 			<div className="">
 				{
 					MenuItems?.itemCards?.map((Items) => <CuisineCard key={Items?.card?.info?.id} Items={Items?.card?.info}></CuisineCard>)
 				}
 			</div>
+
+            <div className="h-4 bg-gray-200 w-full"></div>
+
 		</div>
 
 
